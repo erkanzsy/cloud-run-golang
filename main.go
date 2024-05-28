e699408cded160467057c585ea55da1f8610129d
erkanozsoy <erkan.ozsoy@bil.omu.edu.tr>
2024-05-28 23:35:16 +0300

    test

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World! 12")
+		return c.SendString("Hello, World! 123")
 	})
 
 	app.Listen(":8080")
